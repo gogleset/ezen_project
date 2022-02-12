9142808f642572038da5a518a23730a2af8d59ab
NeoJHS <seul5106@gmail.com>
2022-02-12 15:04:18 +0900

    fix: members.js, menu.html, modification.html, signin.html 1: signin.html-> 로그인 기능구현, 로그인 시 세션데이터 저장 2: members.js-> 회원 로그인 라우터, 회원정보수정시 메뉴에서 현재 접속한 세션데이터를 넘기기 위한 라우터, 회원정보 수정시 받은 세션데이터에서 뽑아낸 회원아이디로 검색하는 라우터, 회원정보 수정 라우터 추가 3: menu.html-> 메뉴에서 회원정보수정 클릭 시 세션데이터에 있는 member_id값을 URL파라미터로 넘겨줌 4: modification.html-> 세션데이터에 저장되어있는 회원정보들을 불러와 인풋태그에 값을 대입한다. 저장하기 버튼을 클릭 시 현재 입력되어있는 데이터들을 db에 저장한다.

--- Node/controllers/members.js
+++ Node/controllers/members.js
@@ -26,7 +26,7 @@ module.exports = (app) => {
         const member_addr2 = req.post("member_addr2");
         const member_birth = req.post("member_birth");
         const admin = req.post("admin");
-       
+
         const is_out = req.post("is_out");
 
         let json = null;
@@ -75,6 +75,156 @@ module.exports = (app) => {
 
     });
 
+    //회원 로그인
+    router.post("/members/login", async (req, res, next) => {
+        const member_id = req.post("member_id");
+        const member_pw = req.post("member_pw");
+
+        try {
+            regexHelper.value(member_id, "아이디를 입력하세요");
+            regexHelper.value(member_pw, "비밀번호를 입력하세요");
+        } catch (e) {
+            return next(e);
+        }
+
+        let json = null;
+
+        try {
+            dbcon = await mysql2.createConnection(config.database);
+            await dbcon.connect();
+
+            let sql = "select member_code, member_id, member_email, member_pw, member_name, member_phone, member_postcode, member_addr1, member_addr2, ";
+            sql += "member_birth, admin, reg_date, is_out ";
+            sql += "from members where member_id =? and member_pw =?"
+            let args1 = [member_id, member_pw];
+
+            const [result] = await dbcon.query(sql, args1);
+
+            json = result;
+
+            // login_date값을 now()로 update 처리(json데이터는 result가 가져가는게 맞다.) reg_date값이 없다아
+            /* let sql2 = "UPDATE members SET login_date=now() WHERE id=?";
+            dbcon.query(sql2, json[0].id);*/
+        } catch (e) {
+            return next(e);
+        } finally {
+            dbcon.end();
+        }
+
+        if (json == null || json.length == 0) {
+            return next(
+                new BadRequestException("아이디나 비밀번호가 잘못되었습니다.")
+            );
+        }
+
+        req.session.memberInfo = json[0];
+
+        res.sendJson();
+    });
+
+    //회원정보수정시 메뉴에서 현재 접속한 세션데이터를 넘기기위한 라우터
+    router.post("/members/session", async (req, res, next) => {
+        if (!req.session.memberInfo) {
+            return next(new BadRequestException("로그인 중이 아닙니다."));
+        }
+        // DB세션에 저장된 데이터 가져오기
+        let sessionInfo = req.session.memberInfo
+
+        let json = null;
+
+        try {
+            dbcon = await mysql2.createConnection(config.database);
+            await dbcon.connect();
+
+
+            let sql = "select member_code, member_id, member_email, member_pw, member_name, member_phone, member_postcode, member_addr1, member_addr2, ";
+            sql += "member_birth, admin, reg_date, is_out ";
+            sql += "from members where member_id =? and member_pw =?"
+            const [result1] = await dbcon.query(sql, [sessionInfo.member_id, sessionInfo.member_pw]);
+
+            json = result1;
+
+        } catch (err) {
+            return next(err);
+        }
+        res.sendJson({ 'item': json });
+    });
+
+    //회원정보 수정시 받은 세션데이터에서 뽑아낸 회원아이디로 검색
+    router.get("/members/set/:member_id", async (req, res, next) => {
+        const member_id = req.get("member_id");
+
+        if (member_id === undefined) {
+            return next(new Error(400));
+        }
+
+        let json = null;
+        try {
+            dbcon = await mysql2.createConnection(config.database);
+            await dbcon.connect();
+
+            let sql = "select member_id, member_email, member_pw, member_name, member_phone, ";
+            sql += "member_postcode, member_addr1, member_addr2, member_birth ";
+            sql += "from members where member_id =?";
+
+            const [result] = await dbcon.query(sql, [member_id]);
+
+            json = result;
+        } catch (err) {
+            return next(err);
+        } finally {
+            dbcon.end();
+        }
+
+        res.sendJson({ item: json })
+    });
+
+    router.put('/members/edit/:member_id', async (req, res, next) => {
+        const member_id = req.get("member_id");
+        const member_email = req.post("member_email");
+        const member_pw = req.post("member_pw");
+        const member_name = req.post("member_name");
+        const member_phone = req.post("member_phone");
+        const member_postcode = req.post("member_postcode");
+        const member_addr1 = req.post("member_addr1");
+        const member_addr2 = req.post("member_addr2");
+        const member_birth = req.post("member_birth");
+
+        if (member_id == null) {
+            return next(new Error(400));
+        }
+
+        let json = null;
+
+        try {
+            dbcon = await mysql2.createConnection(config.database);
+            await dbcon.connect();
+
+            let sql = "UPDATE members SET member_email = ?, member_pw = ?, member_name = ?, member_phone = ?, member_postcode =?, "
+            sql += "member_addr1 =?, member_addr2 =?, member_birth =? WHERE member_id=?"
+
+            const input_data = [member_email, member_pw, member_name, member_phone, member_postcode, member_addr1, member_addr2, member_birth, member_id]
+            const [result] = await dbcon.query(sql, input_data);
+
+            if (result.affectedRows < 1) {
+                throw new Error("수정된 데이터가 없습니다.");
+            }
+
+            json = result;
+        } catch (err) {
+            return next(err);
+        } finally {
+            dbcon.end();
+        }
+
+        res.sendJson({ 'item': json});
+    });
+
+
+    //관리자 로그인
+    router.post("/admin/login", async (req, res, next) => {
+
+    });
 
     return router;
 }
