ce692f196a1ceb493aeb134e154f0c6e6581b6e7
limdj <dazoo0221@gmail.com>
2022-02-15 09:28:15 +0900

    Fix : Event / QnA
    1. Event.js : 파일 업로드, 특정 데이터 출력을 위한 라우터 생성 및 오류 수정
    2. Qna.js : 특정 조회 데이터 데이터 출력을 위한 라우터 생성 및 PATH 경로 수정
    3. *.css : 각 태그 영역 margin, ellipsis 등 일부 css 항목 수정
    4. main.html : 이벤트 배너 이미지 출력 기능 구현
    5. event.html : 파일 업로드 및 섬네일 이미지 출력 기능 구현, 게시 여부 설정값에 따른 배너 이미지 출력 기능 구현
    6. qa.html : 관리자 답변 유무에 따른 UI 변경 기능 추가

--- Node/controllers/QnA.js
+++ Node/controllers/QnA.js
@@ -1,9 +1,8 @@
 /**
- * @filename  : QnA.js
+ * @filename  : Qna.js
  * @author    : 임다정 (dazoo0221@gmail.com)
- * @description : QnA DB연동
+ * @description : Qna Controller
  **/
-
 
 /** 모듈 참조 */
 const axios = require("axios");
@@ -18,7 +17,7 @@ const regexHelper = require("../../helper/regex_helper.js");
 module.exports = (app) => {
     let dbcon = null;
 
-    //데이터 조회
+    //데이터 조회 [관리자 사이트에서 활용]
     router.get('/qna', async (req, res, next) => {
 
         // 검색어 파라미터 받기
@@ -78,11 +77,9 @@ module.exports = (app) => {
         res.sendJson({ 'pagenation': pagenation, 'item': json });
     });
 
-
-
-    // 특정 데이터 조회  --> 본인이 작성한 글 가져오기
-    router.get('/qna/:qna_code', async (req, res, next) => {
-        const qnaCode = req.get('qna_code')
+    // 특정 데이터 조회  
+    router.get('/qna/:qnaNm', async (req, res, next) => {
+        const qnaNm = req.get('qnaNm')
 
         // DB세션에 저장된 데이터 가져오기
         let sessionInfo = req.session.memberInfo
@@ -93,15 +90,15 @@ module.exports = (app) => {
             dbcon = await mysql2.createConnection(config.database);
             await dbcon.connect();
 
-            // qnas 테이블에서 세션에 저장된 ID와 일치하는 게시글 조회하는 sql문
+            // qnas 테이블에서 qna_code 와 일치하는 dt 조회
             let sql1 = "SELECT q.qna_code, qna_title, qna_desc, qna_answer, qna_state, qna_date, m.member_id FROM qnas q inner join members m on q.member_code = m.member_code where qna_code = ?";
 
-            // 사용자 qna에서 확인 시 본인의 게시글만 확인되어야하기때문에 sql문에 AND 연산자 추가하여 검색 조건을 추가함 sessionInfo.member_id 
-            if (qnaCode != null) {
-                sql1 += " AND m.member_id = sessionInfo.member_id";
+            // 
+            if (sessionInfo != null) {
+                sql1 += " AND m.member_id = 'woody'";  // 테스트 중인 값....
             }
 
-            const [result1] = await dbcon.query(sql1, qnaCode);
+            const [result1] = await dbcon.query(sql1, qnaNm);
 
             json = result1;
 
@@ -113,16 +110,45 @@ module.exports = (app) => {
         res.sendJson({ 'item': json });
     });
 
+    // 특정 데이터 조회  -->  세션 DT 조회 (사용자 본인 게시물 조회용)
+    router.get('/myboard', async (req, res, next) => {
 
-    // 데이터 추가
+        // DB세션에 저장된 데이터 가져오기
+        let sessionInfo = req.session.memberInfo
+
+        let json = null;
+
+        try {
+            dbcon = await mysql2.createConnection(config.database);
+            await dbcon.connect();
+
+            // qnas 테이블에서 세션에 저장된 ID와 일치하는 게시글 조회하는 sql문
+            let sql1 = "SELECT q.qna_code, qna_title, qna_desc, qna_answer, qna_state, qna_date, m.member_id FROM qnas q inner join members m on q.member_code = m.member_code where m.member_code = '1'";
+
+            const [result1] = await dbcon.query(sql1); //(sql1, sessionInfo.member_code) 
+
+            json = result1;
+
+            logger.debug(json)
+
+        } catch (err) {
+            return next(err);
+        } finally {
+            dbcon.end();
+        }
+
+        logger.debug(json)
+        logger.debug('연결끄으으읏~!~!~!')
+        res.sendJson({ 'item': json });
+    });
+
+
+    // 데이터 추가 [회원 게시판에서 작성]
     router.post('/qna', async (req, res, next) => {
 
         const qnaTiTle = req.post('qna_title');
         const qnaDesc = req.post('qna_desc');
-        const qnaAnswer = req.post('qna_answer');
-        const qnaState = req.post('qna_state');
-        const qnaDate = req.post('qna_date');
-        const memberCode = req.post('member_code');
+        const memberCode = req.post('member_code'); // 
 
         try {
             regexHelper.value(qnaTiTle, '제목을 입력해주세요.');
@@ -139,12 +165,12 @@ module.exports = (app) => {
             await dbcon.connect();
 
             // 데이터 저장
-            const sql = 'INSERT INTO qnas (qna_title, qna_desc, qna_answer, qna_state, qna_date, member_code) values (?, ?, ?, ?, ?, ?)';
-            const input_data = [qnaTiTle, qnaDesc, qnaAnswer, qnaState, qnaDate, memberCode];
+            const sql = 'INSERT INTO qnas (qna_title, qna_desc, qna_state, qna_date, member_code) values ( ?, ?, "N", now(), ?)';
+            const input_data = [qnaTiTle, qnaDesc, memberCode];
             const [result1] = await dbcon.query(sql, input_data);
 
             // 저장한 데이터를 출력
-            const sql2 = 'SELECT qna_code, qna_title, qna_desc, qna_answer, qna_state, qna_date, member_code FROM event WHERE qna_code=?';
+            const sql2 = 'SELECT qna_code, qna_title, qna_desc, qna_answer, qna_state, qna_date, member_code FROM qnas WHERE qna_code=?';
             const [result2] = await dbcon.query(sql2, [result1.insertId])
 
             json = result2;
@@ -161,8 +187,8 @@ module.exports = (app) => {
 
 
     // 데이터 수정
-    router.put('/qna/:qna_code', async (req, res, next) => {
-        const qnaCode = req.get('qna_code');
+    router.put('/qna/:qnaNm', async (req, res, next) => {
+        const qnaNm = req.get('qnaNm');
         const qnaTiTle = req.post('qna_title');
         const qnaDesc = req.post('qna_desc');
         const qnaAnswer = req.post('qna_answer');
@@ -170,22 +196,21 @@ module.exports = (app) => {
         const qnadate = req.post('qna_date');
         const memberCode = req.post('member_code');
 
-        if (qnaCode === null || qnaTiTle === null || qnaDesc === null || qnaState === null) {
+        if (qnaNm === null || qnaTiTle === null || qnaDesc === null || qnaState === null) {
             return next(new Error(400));
         }
 
-        // 유효성 검사
+        // 유효성 검사 -> 구현 필요
 
-        try{
-            regexHelper.value(qnaTiTle, '제목 값이 없습니다.'); 
-            regexHelper.value(qnaDesc, '내용 값이 없습니다.'); 
-            regexHelper.value(qnaState, '상태 값이 없습니다.'); 
-            regexHelper.value(qnadate, '날짜 값이 없습니다.'); 
+        try {
+            regexHelper.value(qnaTiTle, '제목 값이 없습니다.');
+            regexHelper.value(qnaDesc, '내용 값이 없습니다.');
+            regexHelper.value(qnaState, '상태 값이 없습니다.');
 
-            regexHelper.maxLength(qnaTiTle, 20, '제목이 너무 깁니다.'); 
-            regexHelper.maxLength(qnaDesc, 255, '내용이 너무 깁니다.'); 
+            regexHelper.maxLength(qnaTiTle, 20, '제목이 너무 깁니다.');
+            regexHelper.maxLength(qnaDesc, 255, '내용이 너무 깁니다.');
 
-        }catch(err){
+        } catch (err) {
             return next(err);
         };
 
@@ -196,7 +221,7 @@ module.exports = (app) => {
             await dbcon.connect();
 
             const sql = `UPDATE qnas SET qna_title=?, qna_desc=?, qna_answer=?, qna_state=? WHERE qna_code=?`
-            const input_data = [qnaTiTle, qnaDesc, qnaAnswer, qnaState, qnaCode];
+            const input_data = [qnaTiTle, qnaDesc, qnaAnswer, qnaState, qnaNm];
             const [result1] = await dbcon.query(sql, input_data);
 
             if (result1.affectedRows < 1) {
@@ -213,10 +238,10 @@ module.exports = (app) => {
 
 
     // 데이터 삭제
-    router.delete('/qna/:qna_code', async (req, res, next) => {
-        const qnaCode = req.get('qna_code');
+    router.delete('/qna/:qnaNm', async (req, res, next) => {
+        const qnaNm = req.get('qnaNm');
 
-        if (qnaCode === null) {
+        if (qnaNm === null) {
             return next(new Error(400));
         }
 
@@ -227,7 +252,7 @@ module.exports = (app) => {
 
             const sql = 'DELETE FROM qnas WHERE qna_code = ?'
 
-            const [result1] = await dbcon.query(sql, qnaCode);
+            const [result1] = await dbcon.query(sql, qnaNm);
 
 
             if (result1.affectedRows < 1) {
