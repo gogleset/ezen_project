aab4971557a06b14a95dbbafb2ac7e60ea011982
gogleset <google1130@naver.com>
2022-02-20 23:34:13 +0900

    사용자 페이지 > 장바구니 상품 중복데이터 감지 및 업데이트 구현 #16

--- backend/controllers/Cart.js
+++ backend/controllers/Cart.js
@@ -12,6 +12,7 @@ const logger = require("../helper/LogHelper");
 const config = require("../helper/_config");
 const utilHelper = require("../helper/UtillHelper");
 const regexHelper = require("../helper/regex_helper.js");
+const BadRequestException = require("../exceptions/BadRequestException");
 
 module.exports = (app) => {
   let dbcon = null;
@@ -71,9 +72,10 @@ module.exports = (app) => {
   router.post("/cart", async (req, res, next) => {
     const ctPdCnt = req.post("product_count");
     const ctPdCode = req.post("product_code");
-    const ctMemCode = req.post("member_code");
 
     const sessionInfo = req.session.memberInfo;
+    // 세션값 가져오기
+    const ctMemCode = sessionInfo.member_code;
     console.log(sessionInfo);
     logger.debug(`카트상품개수 ${ctPdCnt}`);
     logger.debug(`카트상품코드 ${ctPdCode}`);
@@ -92,22 +94,30 @@ module.exports = (app) => {
       dbcon = await mysql2.createConnection(config.database);
       await dbcon.connect();
 
-      // 데이터 저장
-      const sql1 =
+      // 회원 장바구니 추가 시 중복 데이터 조회(상품명 코드, 회원코드 일치하는지 확인하고 그 값을 카운트)
+      let sql1 = "SELECT COUNT(*) as cnt";
+      sql1 += " FROM carts";
+      //   sql1 += " INNER JOIN products p ON c.product_code = p.product_code";
+      //   sql1 += " INNER JOIN members m ON c.member_code = m.member_code";
+      sql1 += " WHERE member_code = ? AND product_code = ?";
+
+      let args = [];
+      args.push(ctMemCode);
+      args.push(ctPdCode);
+
+      const [result1] = await dbcon.query(sql1, args);
+      console.log(result1[0].cnt);
+      // 중복값이 있을 경우
+      if (result1[0].cnt > 0) {
+        //   프론트에게 메세지 중복값이 있다는 메세지를 보냄
+        return res.status(200).send("update");
+      }
+
+      // 중복값이 없을 경우 데이터 저장
+      const sql2 =
         "INSERT INTO carts (product_count, product_code, member_code) values (?, ?, ?)";
       const input_data = [ctPdCnt, ctPdCode, ctMemCode];
-      const [result1] = await dbcon.query(sql1, input_data);
-
-      // 회원 장바구니 추가 시 중복 데이터 조회
-      let sql2 = "SELECT p.product_name, m.member_id, COUNT(*) as cnt";
-      sql2 += " FROM carts c";
-      sql2 += " INNER JOIN products p ON c.product_code = p.product_code";
-      sql2 += " INNER JOIN members m ON c.member_code = m.member_code";
-      sql2 += " WHERE m.member_id = ?";
-      sql2 += " GROUP BY p.product_name, m.member_id";
-
-      const [result2] = await dbcon.query(sql2, sessionInfo.member_id);
-      console.log(result2);
+      const [result2] = await dbcon.query(sql2, input_data);
     } catch (err) {
       return next(err);
     } finally {
@@ -121,11 +131,13 @@ module.exports = (app) => {
     const ctCode = req.get("no");
     const ctPdCnt = req.post("product_count");
     const ctPdCode = req.post("product_code");
-    const ctMemCode = req.post("member_code");
+    const sessionInfo = req.session.memberInfo;
+    // 세션값 가져오기
+    const ctMemCode = sessionInfo.member_code;
 
     if (
       ctCode === null ||
-      ctPdCnt === null ||
+      //   ctPdCnt === null ||
       ctPdCode === null ||
       ctMemCode === null
     ) {
@@ -133,23 +145,59 @@ module.exports = (app) => {
     }
 
     let json = null;
+    // 상품의 갯수가 저장될 값
+    let count = null;
+    // URL 파라미터가 num이면(ProductDetail.html 장바구니)
+    if (ctCode == "num") {
+      try {
+        dbcon = await mysql2.createConnection(config.database);
+        await dbcon.connect();
 
-    try {
-      dbcon = await mysql2.createConnection(config.database);
-      await dbcon.connect();
+        let sql1 = "SELECT product_count as count";
+        sql1 += " FROM carts";
+        // sql1 += " INNER JOIN products p ON c.product_code = p.product_code";
+        // sql1 += " INNER JOIN members m ON c.member_code = m.member_code";
+        sql1 += " WHERE member_code = ? AND product_code = ?";
 
-      const sql =
-        "UPDATE carts SET product_count=?, product_code=?, member_code=? WHERE cart_code=?";
-      const input_data = [ctPdCnt, ctPdCode, ctMemCode, ctCode];
-      const [result1] = await dbcon.query(sql, input_data);
+        const input_data = [ctMemCode, ctPdCode];
+        const [result1] = await dbcon.query(sql1, input_data);
+        console.log("수정전 카운터:::" + result1[0].count);
+        // 전역변수 count에 업데이트 할 숫자값을 넣어줍니다.
+        count = result1[0].count + 1;
 
-      if (result1.affectedRows < 1) {
-        throw new Error(" 수정된 데이터가 없습니다. ");
+        let sql2 = "UPDATE carts SET product_count = ?";
+        sql2 += " WHERE member_code = ? AND product_code = ?";
+
+        const args = [count, ctMemCode, ctPdCode];
+        // args.push(count);
+        // args.push(ctMemCode);
+        // args.push(ctPdCode);
+        const [result2] = await dbcon.query(sql2, args);
+        console.log(result2);
+      } catch (error) {
+        next(error);
+      } finally {
+        dbcon.end();
       }
-    } catch (err) {
-      return next(err);
-    } finally {
-      dbcon.end();
+      res.sendJson({ item: json });
+    } else {
+      try {
+        dbcon = await mysql2.createConnection(config.database);
+        await dbcon.connect();
+
+        const sql =
+          "UPDATE carts SET product_count=?, product_code=?, member_code=? WHERE cart_code=?";
+        const input_data = [ctPdCnt, ctPdCode, ctMemCode, ctCode];
+        const [result1] = await dbcon.query(sql, input_data);
+
+        if (result1.affectedRows < 1) {
+          throw new Error(" 수정된 데이터가 없습니다. ");
+        }
+      } catch (err) {
+        return next(err);
+      } finally {
+        dbcon.end();
+      }
     }
     res.sendJson({ item: json });
   });
